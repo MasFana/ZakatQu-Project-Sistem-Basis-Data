499994df46c34a110b54c9c5dc648e1d4e27f6be
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-05-20 11:52:32 +0700

    update

--- ZakatMainProgram/Zakatku/main.py
+++ ZakatMainProgram/Zakatku/main.py
@@ -8,11 +8,9 @@ if __name__ == '__main__':
         terminal.clear_screen()
 
         list_menu: List[str] = ["Login", "Amil zakat", "Pembayaran Zakat", "Distribusi", "Pemberi Zakat", "Penerima Zakat", "Keluar dari aplikasi"]
-
         list.print_list(list_menu)
 
         result: str = input("Pilih menu yang dituju : ")
-
         switcher.switcher_menu(result)
 
 
