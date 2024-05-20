c0690422b5497a829d46875c6e233b632490d3df
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-05-20 11:43:17 +0700

    topikontol

--- ZakatMainProgram/Zakatku/main.py
+++ ZakatMainProgram/Zakatku/main.py
@@ -4,14 +4,16 @@ from components import list
 
 
 if __name__ == '__main__':
+    while(True) :
+        terminal.clear_screen()
 
-    list_menu: List[str] = ["Login", "Amil zakat", "Pembayaran Zakat", "Distribusi", "Pemberi Zakat", "Penerima Zakat"]
+        list_menu: List[str] = ["Login", "Amil zakat", "Pembayaran Zakat", "Distribusi", "Pemberi Zakat", "Penerima Zakat", "Keluar dari aplikasi"]
 
-    list.print_list(list_menu)
+        list.print_list(list_menu)
 
-    result: str = input("Pilih menu yang dituju : ")
+        result: str = input("Pilih menu yang dituju : ")
 
-    switcher.switcher_menu(result)
+        switcher.switcher_menu(result)
 
 
 
