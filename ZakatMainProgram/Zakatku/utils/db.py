8ad4faf9abbef0e9387abbbb42ae06e8639e85f3
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-05-20 14:21:28 +0700

    update

--- ZakatMainProgram/Zakatku/utils/db.py
+++ ZakatMainProgram/Zakatku/utils/db.py
@@ -4,6 +4,8 @@ conn = psycopg2.connect(database='Zakatqu', user='postgres', password='rendydp42
 
 cur = conn.cursor()
 
+
+
 def login_query(username: str, password: str) -> any :
     cur.execute(f"SELECT * FROM amil_zakat WHERE left(nik, 5)= '{username}' AND right(nik, 5)= '{password}';")
     data = cur.fetchall()
