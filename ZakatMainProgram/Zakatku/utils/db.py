83b582c0e5cae49948aaaf9dede10459809eeeea
Muhammad Taufiq Rohman <rohmantaufik327@gmail.com>
2024-05-21 10:19:46 +0700

    Menambahkan fitur create data pembayaran (but amil masih static)

--- ZakatMainProgram/Zakatku/utils/db.py
+++ ZakatMainProgram/Zakatku/utils/db.py
@@ -1,9 +1,8 @@
 import psycopg2
 
-conn = psycopg2.connect(database='Zakatqu', user='postgres', password='rendydp424', host='localhost', port=5432)
+conn = psycopg2.connect(database='ZakatQu', user='postgres', password='19Januari', host='localhost', port=5432)
 
 cur = conn.cursor()
-
 
 
 def login_query(username: str, password: str) -> any :
@@ -14,3 +13,32 @@ def login_query(username: str, password: str) -> any :
     # conn.close()
 
     return data
+
+def QueryInput(InputQuery, NamaTabel, NamaKolom):
+    cur.execute("INSERT INTO " + NamaTabel + " " + f'({NamaKolom})' + " VALUES " + f'{InputQuery}'.replace("[", "(").replace("]", ")"))
+    conn.commit()
+
+def searchPemberi(Input):
+    cur.execute(f"SELECT * FROM pemberi_zakat where nama_pemberi_zakat = '{Input}';")
+    data = cur.fetchall()
+    if data:
+        return data
+    else:
+        return "Data Tidak Ada"
+
+def searchBentukZakat(Input):
+    cur.execute(f"SELECT * FROM bentuk_zakat where id_bentuk_zakat = '{Input}';")
+    data = cur.fetchall()
+    if data:
+        return data
+    else:
+        return "Data Tidak Ada"
+    
+def searchJenisZakat(Input):
+    cur.execute(f"SELECT * FROM jenis_zakat where id_jenis_zakat = '{Input}';")
+    data = cur.fetchall()
+    if data:
+        return data
+    else:
+        return "Data Tidak Ada"
+    
